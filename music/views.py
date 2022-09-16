b24fdb82cad4578c5cc9525390078adba437d0a7
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-09-16 15:31:02 -0600

    created get functionality

--- music/views.py
+++ music/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import MusicSerializer
+from .models import Music
+
 
 # Create your views here.
 
-def music_list():
-    pass
+@api_view(['GET'])
+def music_list(request):
+    if request.method == 'GET':
+        music = Music.objects.all()
+        serializer = MusicSerializer(music, many=True)
+        return Response(serializer.data)
+
 
 def music_detail():
     pass

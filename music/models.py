47319b15237c00cf09f7e83599fd1c2417f7c2c7
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-09-16 13:42:13 -0600

    created music model

--- music/models.py
+++ music/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Music(models.Model):
+    title = models.CharField(max_length=200)
+    artist = models.CharField(max_length=200)
+    album = models.CharField(max_length=100)
+    release_date = models.DateField()
+    genre = models.CharField(max_length=100)

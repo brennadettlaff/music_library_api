4098e2c9d579b515e4cd130bcc381b9ca3200a53
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-09-19 13:49:29 -0600

    created get by id functionality

--- music/views.py
+++ music/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -20,6 +21,9 @@ def music_list(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     
 
-
-def music_detail():
-    pass
+@api_view(['GET'])
+def music_detail(request, pk):
+    music = get_object_or_404(Music, pk=pk)
+    if request.method == 'GET':
+        serializer = MusicSerializer(music)
+        return Response(serializer.data)

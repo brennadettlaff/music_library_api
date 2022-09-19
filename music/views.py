175c5d81f1e369d9545b84ac0d2b2f44a0c15f62
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-09-19 13:36:29 -0600

    added post functionality

--- music/views.py
+++ music/views.py
@@ -1,17 +1,23 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import MusicSerializer
 from .models import Music
 
 
 # Create your views here.
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def music_list(request):
     if request.method == 'GET':
         music = Music.objects.all()
         serializer = MusicSerializer(music, many=True)
         return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = MusicSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
     
 
 
